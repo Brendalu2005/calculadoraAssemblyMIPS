7809442483838f2233a92313ec2d969b7b59adad
Brendalu2005 <brenda.luanacb@gmail.com>
2025-12-01 19:45:22 -0300

    finalizando funcionalidade

--- calculadoraFinal.asm
+++ calculadoraFinal.asm
@@ -3,8 +3,9 @@ strAutor: .asciiz "Brenda Luana Correia Bezerra (Blcb)\n"
 strData: .asciiz "Data de Criacao: 30/11/2025\n"
 strRevisao1: .asciiz "commit 1 (30/11/2025 - 20:33): Implementacao de Base 2, 8 e 16.\n"
 strRevisao2: .asciiz "commit 2:(01/12/2025 - 15:26): Implementacao de BCD e Complemento de 2 (16 bits)\n"
-strRevisao3: .asciiz "commit 3:..\n"
+strRevisao3: .asciiz "commit 3:(01/12/2025 - 19:42): Implementacao de Real para Float e Double (IEEE 754)\n"
 
+.data
 menu: .asciiz "\nCALCULADORA PROGRAMADOR DIDATICA\n"
 menu1: .asciiz "1 - Converter Decimal (Base 10) para 2, 8, 16 e BCD\n"
 menu2: .asciiz "2 - Converter Decimal (Base 10) para Complemento de 2 (16 bits)\n"
@@ -12,11 +13,20 @@ menu3: .asciiz "3 - Converter Real (Decimal) para Float e Double (IEEE 754)\n"
 menu4: .asciiz "4 - Sair\n"
 menu5: .asciiz "Escolha uma opcao: "
 promptNum: .asciiz "Digite o numero inteiro Decimal (Base 10): "
+promptFloat: .asciiz "Digite o numero Real Decimal: "
 msgBin: .asciiz "\n--- CONVERSAO PARA BINARIO (BASE 2) ---\n"
 msgOct: .asciiz "\n--- CONVERSAO PARA OCTAL (BASE 8) ---\n"
 msgHex: .asciiz "\n--- CONVERSAO PARA HEXADECIMAL (BASE 16) ---\n"
 msgBCD: .asciiz "\n--- CONVERSAO PARA BCD (Binary-Coded Decimal) ---\n"
 msgC2: .asciiz "\n--- CONVERSAO PARA COMPLEMENTO DE 2 (16 BITS) ---\n"
+msgFloat: .asciiz "\n--- CONVERSAO PARA FLOAT (32 BITS - IEEE 754) ---\n"
+msgDouble: .asciiz "\n--- CONVERSAO PARA DOUBLE (64 BITS - IEEE 754) ---\n"
+msgFloatBin: .asciiz "Bits (32 bits): "
+msgDoubleBin: .asciiz "Bits (64 bits): "
+msgSinal: .asciiz "SINAL (Bit 31/63): "
+msgExpoente: .asciiz "EXPOENTE (Decimal): "
+msgExpoenteVies: .asciiz "EXPOENTE COM VIES (Binario/Decimal): "
+msgFracao: .asciiz "FRACAO (Mantissa): "
 msgDiv: .asciiz " / "
 msgResto: .asciiz " = "
 msgIgualResto: .asciiz ", Resto: "
@@ -35,6 +45,8 @@ buffer_hex: .space 9
 buffer_oct: .space 12
 buffer_bcd: .space 40
 buffer_c2: .space 17
+buffer_32bit: .space 33
+buffer_64bit: .space 65
 zero_char: .asciiz "0"
 newline: .asciiz "\n"
 
@@ -127,467 +139,365 @@ jal to_comp2
 j loop_menu
 
 conversao3:
+li $v0, 4
+la $a0, promptFloat
+syscall
+    
+li $v0, 6
+syscall
+    
+jal to_float_32bit
+    
+li $v0, 7
+syscall
+    
+jal to_double_64bit
+    
 j loop_menu
 
-to_bin:
+to_float_32bit:
 addi $sp, $sp, -4
 sw $ra, 0($sp)
     
 li $v0, 4
-la $a0, msgBin
-syscall
-
-move $t0, $a0
-la $t1, buffer_bin
-    
-addi $t1, $t1, 31
-sb $zero, ($t1)
-addi $t1, $t1, -1
-
-loop_bin_div:
-beq $t0, $zero, end_bin_div
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgDiv
-syscall
-li $a0, 2
-syscall
-la $a0, msgResto
-syscall
-
-addi $t3, $t0, 0
-li $t4, 2
-div $t3, $t4
-mflo $t0
-mfhi $t5
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgIgualResto
+la $a0, msgFloat
 syscall
     
-addi $t6, $t5, 48
-sb $t6, ($t1)
-    
-li $v0, 1
-move $a0, $t5
-syscall
-li $v0, 11
-li $a0, 10
-syscall
-
-addi $t1, $t1, -1
-j loop_bin_div
-
-end_bin_div:
-li $v0, 4
-la $a0, msgFinal
-syscall
-    
-addi $t1, $t1, 1
-li $v0, 4
-move $a0, $t1
-syscall
-    
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
-
-to_oct:
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-
-li $v0, 4
-la $a0, msgOct
-syscall
-
-move $t0, $a0
-la $t1, buffer_oct
-addi $t1, $t1, 11
-sb $zero, ($t1)
-addi $t1, $t1, -1
-
-loop_oct_div:
-beq $t0, $zero, end_oct_div
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgDiv
-syscall
-li $a0, 8
-syscall
-la $a0, msgResto
-syscall
-
-addi $t3, $t0, 0
-li $t4, 8
-div $t3, $t4
-mflo $t0
-mfhi $t5
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgIgualResto
-syscall
-    
-addi $t6, $t5, 48
-sb $t6, ($t1)
-    
-li $v0, 1
-move $a0, $t5
-syscall
-li $v0, 11
-li $a0, 10
-syscall
-
-addi $t1, $t1, -1
-j loop_oct_div
-
-end_oct_div:
-li $v0, 4
-la $a0, msgFinal
-syscall
-    
-addi $t1, $t1, 1
-li $v0, 4
-move $a0, $t1
-syscall
-    
-li $v0, 11
-li $a0, 10
-syscall
-
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
-
-to_hex:
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-
-li $v0, 4
-la $a0, msgHex
-syscall
-
-move $t0, $a0
-la $t1, buffer_hex
-addi $t1, $t1, 8
-sb $zero, ($t1)
-addi $t1, $t1, -1
-
-loop_hex_div:
-beq $t0, $zero, end_hex_div
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgDiv
-syscall
-li $a0, 16
-syscall
-la $a0, msgResto
-syscall
-
-addi $t3, $t0, 0
-li $t4, 16
-div $t3, $t4
-mflo $t0
-mfhi $t5
-
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, msgIgualResto
-syscall
-    
-li $t6, 10
-blt $t5, $t6, resto_num
-    
-addi $t6, $t5, 55
-j resto_store
-
-resto_num:
-addi $t6, $t5, 48
-
-resto_store:
-sb $t6, ($t1)
-    
-li $v0, 1
-move $a0, $t5
-syscall
-li $v0, 11
-li $a0, 10
-syscall
-
-addi $t1, $t1, -1
-j loop_hex_div
-
-end_hex_div:
-li $v0, 4
-la $a0, msgFinal
-syscall
-    
-addi $t1, $t1, 1
-li $v0, 4
-move $a0, $t1
-syscall
-    
-li $v0, 11
-li $a0, 10
-syscall
-
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
-
-to_bcd:
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-
-li $v0, 4
-la $a0, msgBCD
-syscall
-    
-move $t0, $a0
-la $t1, buffer_bcd
-li $t2, 10
-    
-addi $sp, $sp, -12
-sw $s0, 0($sp)
-sw $s1, 4($sp)
-sw $s2, 8($sp)
-    
-move $s0, $t0
-li $s1, 0
-    
-loop_bcd_prepare:
-div $s0, $t2
-mflo $s0
-mfhi $s2
-    
-addi $s2, $s2, 48
-addi $sp, $sp, -4
-sb $s2, 0($sp)
-addi $s1, $s1, 1
-    
-bgt $s0, $zero, loop_bcd_prepare
-
-loop_bcd_print:
-beq $s1, $zero, end_bcd
-
-addi $sp, $sp, 4
-lb $t3, 0($sp)
-addi $t3, $t3, -48
-
-li $v0, 1
-move $a0, $t3
-syscall
-li $v0, 4
-la $a0, msgIgualResto
-syscall
-    
-li $t4, 8
-li $t5, 4
-    
-loop_bcd_bits:
-sll $t6, $t3, 0
-and $t6, $t6, $t4
-bne $t6, $zero, bit_one
-li $v0, 11
-li $a0, 48
-syscall
-j next_bit
-
-bit_one:
-li $v0, 11
-li $a0, 49
-syscall
-
-next_bit:
-srl $t4, $t4, 1
-addi $t5, $t5, -1
-bne $t5, $zero, loop_bcd_bits
-    
-li $v0, 11
-li $a0, 32
-syscall
-
-addi $s1, $s1, -1
-j loop_bcd_print
-    
-end_bcd:
-lw $s2, 8($sp)
-lw $s1, 4($sp)
-lw $s0, 0($sp)
-addi $sp, $sp, 12
+mfc1 $t0, $f0
     
 li $v0, 4
-la $a0, newline
-syscall
-    
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
-
-to_comp2:
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-    
-li $v0, 4
-la $a0, msgC2
-syscall
-
-bltz $a0, neg_c2
-
-li $v0, 4
-la $a0, msgC2Positivo
-syscall
-    
-move $t0, $a0
-li $t1, 16
-jal print_16bit_binary
-j end_c2
-
-neg_c2:
-li $v0, 4
-la $a0, msgC2Negativo
-syscall
-    
-li $v0, 4
-la $a0, msgC2Mag
-syscall
-sub $t0, $zero, $a0
-li $v0, 1
-move $a0, $t0
-syscall
-li $v0, 4
-la $a0, newline
-syscall
-    
-li $v0, 4
-la $a0, msgC2Bin
-syscall
-li $t1, 16
-move $a0, $t0
-jal print_16bit_binary_no_print
-move $t0, $s0
-    
-li $v0, 4
-la $a0, msgC2Inv
-syscall
-li $t1, 0xFFFF0000
-nor $t0, $t0, $zero
-andi $t0, $t0, 0xFFFF
-move $a0, $t0
-li $t1, 16
-jal print_16bit_binary
-    
-li $v0, 4
-la $a0, msgC2Add1
-syscall
-addi $t0, $t0, 1
-move $a0, $t0
-li $v0, 1
-syscall
-li $v0, 4
-la $a0, newline
-syscall
-
-li $v0, 4
-la $a0, msgC2Res
+la $a0, msgFloatBin
 syscall
 move $a0, $t0
-li $t1, 16
-jal print_16bit_binary
+li $t1, 32
+la $t2, buffer_32bit
+jal print_bits_from_int_32
     
-end_c2:
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
-
-print_16bit_binary:
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-    
-move $t0, $a0
-move $s0, $t1
-la $t1, buffer_c2
-addi $t1, $t1, 15
-sb $zero, ($t1)
-addi $t1, $t1, -1
-    
-loop_print_bin:
-beq $s0, $zero, end_print_bin
-    
-andi $t2, $t0, 1
-    
-addi $t3, $t2, 48
-sb $t3, ($t1)
-    
-srl $t0, $t0, 1
-addi $t1, $t1, -1
-addi $s0, $s0, -1
-j loop_print_bin
-
-end_print_bin:
-addi $t1, $t1, 1
 li $v0, 4
+la $a0, msgSinal
+syscall
+srl $t1, $t0, 31
+li $v0, 1
 move $a0, $t1
 syscall
 li $v0, 4
 la $a0, newline
 syscall
     
+li $v0, 4
+la $a0, msgExpoenteVies
+syscall
+srl $t1, $t0, 23
+andi $t1, $t1, 0xFF
+li $v0, 1
+move $a0, $t1
+syscall
+li $v0, 4
+la $a0, newline
+syscall
+
+li $v0, 4
+la $a0, msgExpoente
+syscall
+li $t2, 127
+sub $t1, $t1, $t2
+li $v0, 1
+move $a0, $t1
+syscall
+li $v0, 4
+la $a0, newline
+syscall
+
+li $v0, 4
+la $a0, msgFracao
+syscall
+andi $t1, $t0, 0x7FFFFF
+move $a0, $t1
+li $t1, 23
+la $t2, buffer_32bit
+jal print_bits_from_int_23
+    
 lw $ra, 0($sp)
 addi $sp, $sp, 4
 jr $ra
 
-print_16bit_binary_no_print:
+to_double_64bit:
 addi $sp, $sp, -4
 sw $ra, 0($sp)
     
-move $t0, $a0
-move $s0, $t0
-move $t1, $a0
-li $s1, 16
-la $t2, buffer_c2
-addi $t2, $t2, 15
-sb $zero, ($t2)
-addi $t2, $t2, -1
-    
-loop_store_bin:
-beq $s1, $zero, end_store_bin
-    
-andi $t3, $t1, 1
-    
-addi $t4, $t3, 48
-sb $t4, ($t2)
-    
-srl $t1, $t1, 1
-addi $t2, $t2, -1
-addi $s1, $s1, -1
-j loop_store_bin
-
-end_store_bin:
-addi $t2, $t2, 1
 li $v0, 4
+la $a0, msgDouble
+syscall
+    
+mfc1 $t0, $f0
+mfc1 $t1, $f1
+    
+li $v0, 4
+la $a0, msgDoubleBin
+syscall
+move $a0, $t0
+move $a1, $t1
+la $t2, buffer_64bit
+jal print_bits_from_int_64
+    
+li $v0, 4
+la $a0, msgSinal
+syscall
+srl $t2, $t0, 31
+li $v0, 1
 move $a0, $t2
 syscall
 li $v0, 4
 la $a0, newline
 syscall
     
+li $v0, 4
+la $a0, msgExpoenteVies
+syscall
+srl $t2, $t0, 20
+andi $t2, $t2, 0x7FF
+li $v0, 1
+move $a0, $t2
+syscall
+li $v0, 4
+la $a0, newline
+syscall
+
+li $v0, 4
+la $a0, msgExpoente
+syscall
+li $t3, 1023
+sub $t2, $t2, $t3
+li $v0, 1
+move $a0, $t2
+syscall
+li $v0, 4
+la $a0, newline
+syscall
+
+li $v0, 4
+la $a0, msgFracao
+syscall
+    
+andi $t2, $t0, 0xFFFFF
+move $a0, $t2
+li $t3, 20
+la $t4, buffer_32bit
+jal print_bits_from_int_20
+    
+move $a0, $t1
+li $t3, 32
+la $t4, buffer_32bit
+jal print_bits_from_int_32_no_space
+    
+li $v0, 4
+la $a0, newline
+syscall
+
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+print_bits_from_int_32:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+    
+move $t0, $a0
+move $s0, $t1
+move $s1, $t2
+addi $s1, $s1, 31
+sb $zero, ($s1)
+addi $s1, $s1, -1
+    
+li $s2, 0
+    
+loop_print_32bit:
+beq $s0, $zero, end_print_32bit
+    
+andi $t3, $t0, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t0, $t0, 1
+addi $s1, $s1, -1
+addi $s0, $s0, -1
+    
+addi $s2, $s2, 1
+li $t5, 8
+rem $t6, $s2, $t5
+beq $t6, $zero, add_space
+    
+j loop_print_32bit
+
+add_space:
+li $v0, 11
+li $a0, 32
+syscall
+j loop_print_32bit
+
+end_print_32bit:
+li $v0, 4
+la $a0, newline
+syscall
+    
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+print_bits_from_int_23:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+    
+move $t0, $a0
+move $s0, $t1
+move $s1, $t2
+addi $s1, $s1, 23
+sb $zero, ($s1)
+addi $s1, $s1, -1
+    
+loop_print_23bit:
+beq $s0, $zero, end_print_23bit
+    
+andi $t3, $t0, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t0, $t0, 1
+addi $s1, $s1, -1
+addi $s0, $s0, -1
+j loop_print_23bit
+
+end_print_23bit:
+li $v0, 4
+la $a0, newline
+syscall
+    
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+print_bits_from_int_64:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+    
+move $t0, $a0
+move $t1, $a1
+move $s1, $t2
+addi $s1, $s1, 63
+sb $zero, ($s1)
+addi $s1, $s1, -1
+    
+li $s2, 0
+li $s3, 32
+    
+loop_low_word:
+beq $s3, $zero, start_high_word
+    
+andi $t3, $t1, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t1, $t1, 1
+addi $s1, $s1, -1
+addi $s3, $s3, -1
+    
+addi $s2, $s2, 1
+j loop_low_word
+
+start_high_word:
+li $s3, 32
+    
+loop_high_word:
+beq $s3, $zero, end_print_64bit
+    
+andi $t3, $t0, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t0, $t0, 1
+addi $s1, $s1, -1
+addi $s3, $s3, -1
+    
+addi $s2, $s2, 1
+li $t5, 8
+rem $t6, $s2, $t5
+beq $t6, $zero, add_space_64
+    
+j loop_high_word
+
+add_space_64:
+li $v0, 11
+li $a0, 32
+syscall
+j loop_high_word
+
+end_print_64bit:
+li $v0, 4
+la $a0, newline
+syscall
+    
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+print_bits_from_int_20:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+    
+move $t0, $a0
+move $s0, $t1
+move $s1, $t2
+addi $s1, $s1, 20
+sb $zero, ($s1)
+addi $s1, $s1, -1
+    
+loop_print_20bit:
+beq $s0, $zero, end_print_20bit
+    
+andi $t3, $t0, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t0, $t0, 1
+addi $s1, $s1, -1
+addi $s0, $s0, -1
+j loop_print_20bit
+
+end_print_20bit:
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+print_bits_from_int_32_no_space:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+    
+move $t0, $a0
+move $s0, $t1
+move $s1, $t2
+addi $s1, $s1, 32
+sb $zero, ($s1)
+addi $s1, $s1, -1
+    
+loop_print_32_no_space:
+beq $s0, $zero, end_print_32_no_space
+    
+andi $t3, $t0, 1
+    
+addi $t4, $t3, 48
+sb $t4, ($s1)
+    
+srl $t0, $t0, 1
+addi $s1, $s1, -1
+addi $s0, $s0, -1
+j loop_print_32_no_space
+
+end_print_32_no_space:
 lw $ra, 0($sp)
 addi $sp, $sp, 4
 jr $ra
